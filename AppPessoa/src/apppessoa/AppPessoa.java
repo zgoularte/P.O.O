5afddf89b2f2e9d038dd2e3ebb756f11b0605fe3
Goularte G.C <92948398+zgoularte@users.noreply.github.com>
2022-03-23 00:19:38 -0300

    ups

--- AppPessoa/src/apppessoa/AppPessoa.java
+++ AppPessoa/src/apppessoa/AppPessoa.java
@@ -4,11 +4,11 @@ public class AppPessoa {
 
     public static void main(String[] args) {
         Pessoa pess = new Pessoa(10, "Leo", "00000000054", 17);
-        
+
         System.out.println("O código de " + pess.nome + " é " + pess.codigo);
         System.out.println("O cpf de " + pess.nome + " é " + pess.cpf);
-        
+
         System.out.println(pess.nome + " é maior de idade? " + pess.verificaMaiorDeIdade());
     }
-    
+
 }
