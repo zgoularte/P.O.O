0d9ad97f6c851947c8fbd6f5cfc58fd48a87812f
Goularte G.C <92948398+zgoularte@users.noreply.github.com>
2022-03-23 20:26:49 -0300

    up AppForma

---
+++ AppForma/src/appforma/Circulo.java
@@ -0,0 +1,9 @@
+package appforma;
+
+public class Circulo extends Forma {
+
+    @Override
+    public double calculaArea() {
+        return 3.14159 * 10 * 10;
+    }
+}
---
+++ AppForma/src/appforma/Soma.java
@@ -0,0 +1,16 @@
+package appforma;
+
+public class Soma {
+
+    public static int soma(int x, int y) {
+        return x + y;
+    }
+
+    public static String soma(String x, String y) {
+        return x + y;
+    }
+
+    public static double soma(double x, double y) {
+        return x + y;
+    }
+}
