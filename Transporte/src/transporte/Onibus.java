7aad983ba4ea3c5b15b88e6c18f46c52773c9c7d
Goularte G.C <zgoularte_@hotmail.com>
2022-04-09 15:43:26 -0300

    up transporte

---
+++ Transporte/src/transporte/Onibus.java
@@ -0,0 +1,17 @@
+package transporte;
+
+public class Onibus implements Veiculo {
+
+    @Override
+    public void freia() {
+    }
+
+    @Override
+    public void acelera() {
+    }
+
+    @Override
+    public void vira() {
+    }
+
+}
---
+++ Transporte/src/transporte/Veiculo.java
@@ -0,0 +1,10 @@
+package transporte;
+
+public interface Veiculo {
+
+    public void freia();
+
+    public void acelera();
+
+    public void vira();
+}
